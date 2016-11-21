ad30e7f89533436f12169402571f2f51b8917582
apanagio <apanagio@gmail.com>
2016-11-21 15:24:05 +0000

    added AM8

---
+++ AM8/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $("#form1").alpaca({
+        schemaSource: "./schema.json",
+        optionsSource: "./options.json",
+        dataSource: "./data.json",
+        view: "bootstrap-edit-horizontal"
+    });
+
+    $("#download-btn").on('click', function() {
+        var value = $("#form1").alpaca('get').getValue();
+        this.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(value)));
+        this.setAttribute('download', "Υποβολή" + $.format.date(new Date(), 'yyyy_M_d_H_mm_ss') + ".txt");
+    });
+
+    $('#advanced-download-link').on('click', function () {
+        var value = $("#form1").alpaca('get').getValue();
+    	$('#submit-content').val(JSON.stringify(value));
+    });
+
+    $("#upload-file").on('change', function() {
+        var file = this.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+            reader.onload = function(evt) {
+                $('#form1').empty();
+                $("#form1").alpaca({
+                    schemaSource: "./schema.json",
+                    optionsSource: "./options.json",
+                    view: "bootstrap-edit",
+                    data: JSON.parse(evt.target.result)
+                });
+            }
+            reader.onerror = function(evt) {
+
+            }
+        }
+    });
+});
