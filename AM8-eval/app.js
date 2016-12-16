97a3c61aaa1156b1b3eccdbadb3831aea9be4d30
apanagio <apanagio@gmail.com>
2016-12-16 10:43:27 +0000

    tab2 ready

--- AM8-eval/app.js
+++ AM8-eval/app.js
@@ -1,8 +1,23 @@
+/*global $ */
 $(document).ready(function() {
+    $('body').css('cursor', 'progress');
+
     $("#form1").alpaca({
         schemaSource: "./schema.json",
         optionsSource: "./options.json",
         dataSource: "./data.json",
+        postRender: function() {
+            $('body').css('cursor', 'default');
+            $('[data-alpaca-field-name=tab2] table').removeClass('table-hover table-striped');
+            $('[data-alpaca-field-name=tab2] tbody tr[data-alpaca-container-item-index=0] td[data-alpaca-container-item-index=0]').attr('rowspan', 5);
+            $('[data-alpaca-field-name=tab2] tbody tr[data-alpaca-container-item-index=5] td[data-alpaca-container-item-index=0]').attr('rowspan', 2);
+            [1,2,3,4,6].map(function (el) {
+                $('[data-alpaca-field-name=tab2] tbody tr[data-alpaca-container-item-index=' + el + '] td[data-alpaca-container-item-index=0]').remove();
+            });
+            [5,6].map(function (el) {
+                $('[data-alpaca-field-name=tab2] tbody tr[data-alpaca-container-item-index=' + el + ']').addClass('bg-info');
+            })
+        },
         view: {
             parent: "bootstrap-edit-horizontal",
             wizard: {
@@ -11,16 +26,11 @@ $(document).ready(function() {
                 validation: false,
                 hideSubmitButton: true,
                 bindings: {
-                    "tab1": 1,
-                    "tab2": 2,
+                    "tab1": 2,
+                    "tab2": 1,
                     "tab3": 3,
                     "tab4": 4,
-                    "tab5": 5,
-                    "tab6": 6,
-                    "tab7": 7,
-                    "tab8": 8,
-                    "tab9": 9,
-                    "tab10": 10
+                    "tab5": 5
                 },
                 steps: [{
                     title: "1",
@@ -48,9 +58,9 @@ $(document).ready(function() {
         this.setAttribute('download', "Υποβολή" + $.format.date(new Date(), 'yyyy_M_d_H_mm_ss') + ".txt");
     });
 
-    $('#advanced-download-link').on('click', function () {
+    $('#advanced-download-link').on('click', function() {
         var value = $("#form1").alpaca('get').getValue();
-    	$('#submit-content').val(JSON.stringify(value));
+        $('#submit-content').val(JSON.stringify(value));
     });
 
     $("#upload-file").on('change', function() {
