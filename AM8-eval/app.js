f5baf936c69b7a93815ec06be3fc356a351004c8
apanagio <apanagio@gmail.com>
2016-12-20 12:55:39 +0000

    ready

--- AM8-eval/app.js
+++ AM8-eval/app.js
@@ -57,8 +57,62 @@ $(document).ready(function() {
             //tab5
             budgetRules();
             
+            var requirements = function () {
+                return (!$('[data-alpaca-field-id="tab1"] :checkbox').not(':checked').length > 0);
+            };
+            
+            var calcConditions = function () {
+                return $('[data-alpaca-field-id="tab2"] tbody tr').toArray().reduce(function (acc, el) {
+                    var value = $(el).find('[data-alpaca-container-item-index="5"] input').val();
+                    var weight = $(el).find('[data-alpaca-container-item-index="6"] .alpaca-control').text().replace(',', '.');
+                    return acc + (value * weight);
+                }, 0);
+            };
+            
+            var calcInvestment = function () {
+                return $('[data-alpaca-field-id="tab3"] tbody tr').toArray().reduce(function (acc, el, i) {
+                    var value = $(el).find('[data-alpaca-container-item-index="5"] input').val();
+                    var weight = i == 0 ? 0.4 : 0.6;
+                    return acc + (value * weight);
+                }, 0);
+            };
+
+            var calcMaturity = function () {
+                return $('[data-alpaca-field-id="tab4"] tbody tr').toArray().reduce(function (acc, el, i) {
+                    var value = $(el).find('[data-alpaca-container-item-index="5"] input').val();
+                    var weight = i != 5? $(el).find('[data-alpaca-container-item-index="6"] .alpaca-control').text().replace(',', '.') : 0.35;
+                    return acc + (value * weight);
+                }, 0);
+            };
+            
+            var maturityOnOff = function () {
+                return [1, 2].reduce(function (acc, el) {
+                    return acc * $('[data-alpaca-field-id="tab4"] tbody tr[data-alpaca-container-item-index="' + el + '"] [data-alpaca-container-item-index="5"] input').val();
+                }, 1);
+            };
+            
+            var calcTotal = function () {
+                return calcConditions() * 0.3 + calcInvestment() * 0.5 + calcMaturity() * 0.2;
+            };
+            
+            var pass = function () {
+                return requirements && (maturityOnOff() > 0) && (calcTotal() > 4);
+            };
+
             //tab6
-            $('#result').show().appendTo('[data-alpaca-field-name=tab6]');
+            $('#result').show().appendTo('[data-alpaca-field-id=tab6]');
+            $('#calc').on('click', function () {
+                $('#on-off').html( requirements() ? 'ΝΑΙ' : 'ΟΧΙ');
+                $('#condition').html(calcConditions().toFixed(2));    
+                $('#condition-total').html((calcConditions() * 0.3).toFixed(2));    
+                $('#investment-plan').html(calcInvestment().toFixed(2));    
+                $('#investment-plan-total').html((calcInvestment() * 0.5).toFixed(2));    
+                $('#maturity').html(calcMaturity().toFixed(2));    
+                $('#maturity-total').html((calcMaturity() * 0.2).toFixed(2));
+                $('#maturity-on-off').html(maturityOnOff() == 0 ? 'ΟΧΙ' : 'ΝΑΙ');    
+                $('#total').html(calcTotal().toFixed(2));
+                $('#pass').html(pass() ? 'ΝΑΙ' : 'ΟΧΙ');
+            });
             
         },
         view: {
